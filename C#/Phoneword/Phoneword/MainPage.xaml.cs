fcb45e7285b7ad90129c1c967689bf53f05b6816
Secchu <duongchu@hotmail.com>
2024-03-25 02:51:25 +0000

    Added phoneworld app

---
+++ C#/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        private string? translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = PhoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                CallButton.IsEnabled = true;
+                CallButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                CallButton.IsEnabled = false;
+                CallButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            var prompt = $"Would you like to call {translatedNumber}?";
+            if (await DisplayAlert("Dial a Number", prompt, "Yes", "No"))
+            {
+                try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+    }
+
+}
